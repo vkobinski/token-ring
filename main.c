1a7b5413e2ec5d99b467b1e8e162717bc2307f24
vkobinski <victorkobinski@outlook.com>
2024-04-23 22:30:46 -0300

    finished

--- main.c
+++ main.c
@@ -1,23 +1,46 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main() {
 
     int descriptors[2];
     int descriptor = pipe(descriptors);
 
-    int child_pid = fork();
+    if(descriptor != 0) {
+	    perror("Could not create pipe");
+	    exit(1);
+    }
 
-    pid_t my_pid = getpid();
-    pid_t parent_pid = getppid();
+    char first[1000];
+    first[0] = '#';
 
-    write(descriptors[1], "#", 1);
+    for(int i = 1; i < 1000; i++) {
+	    first[i] = '\0';
+    }
 
-    char buf[20];
+    write(descriptors[1], first, 1000);
 
-    read(descriptors[0], buf, 20);
+    for(int i = 0; i < 1000; i++) {
+    	int child_pid = fork();
 
+	    if(child_pid == 0) {
+
+	    	char buf[1000];
+	    	read(descriptors[0], buf, 1000);
+
+	    	size_t size = strlen(buf);
+	    	buf[size] = '0' + ((int)size);
+
+        	write(descriptors[1], buf, 1000);
+	    	exit(0);
+	    }
+
+    }
+
+    char buf[1000];
+    read(descriptors[0], buf, 1000);
     printf("%s\n", buf);
 
     return 0;
